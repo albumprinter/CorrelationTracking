5a50cc12a9618f66af96c348cb4f06bdaeddb4b9
Sergii Kolbasin <sergii.kolbasin@albumprinter.com>
2016-06-14 15:24:20 +0200

    aded the CommonAssemblyInfo.cs

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Albumprinter")]
+[assembly: AssemblyProduct("Albumprinter.CorrelationTracking")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("Albumprinter")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
