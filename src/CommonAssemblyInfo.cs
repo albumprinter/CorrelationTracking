dd1779f5a142b8654f0eaae47c90b150660742a5
Sergii Kolbasin <sergii.kolbasin@albumprinter.com>
2016-06-14 17:43:59 +0200

    added AssemblyInformationalVersion attribute to CommonAssemblyInfo
    in order to build find the version number on building the project in TeamCity

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
@@ -11,3 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
