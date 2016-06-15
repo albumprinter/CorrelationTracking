91c277a06af98cc387bdc8aec69a623a060eecc3
Sergii Kolbasin <sergii.kolbasin@albumprinter.com>
2016-06-15 17:45:02 +0200

    fixed the AssemblyTitle names for Tracing.WCF and Tracing.Http

--- src/Tracing.Http/Properties/AssemblyInfo.cs
+++ src/Tracing.Http/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Albumprinter.CorrelationTracking.Tracing.Http.Log4net")]
+[assembly: AssemblyTitle("Albumprinter.CorrelationTracking.Tracing.Http")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("5bda796a-c6bc-4a0d-9746-56703d448b3b")]
--- src/Tracing.WCF/Properties/AssemblyInfo.cs
+++ src/Tracing.WCF/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Albumprinter.CorrelationTracking.Tracing.WCF.Log4net")]
+[assembly: AssemblyTitle("Albumprinter.CorrelationTracking.Tracing.WCF")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("5349bf26-7f07-4158-b969-979df3bd14a3")]
