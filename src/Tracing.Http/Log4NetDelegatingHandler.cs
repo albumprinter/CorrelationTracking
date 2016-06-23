3c1c608d1267a5cac22fad39d3cfa8f8282bebff
Sergii Kolbasin <sergii.kolbasin@albumprinter.com>
2016-06-23 16:40:22 +0200

    FULFILL-166: Log4NetDelegatingHandler logs only necessary

--- src/Tracing.Http/Log4NetDelegatingHandler.cs
+++ src/Tracing.Http/Log4NetDelegatingHandler.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using log4net;
@@ -11,11 +15,13 @@ namespace Albumprinter.CorrelationTracking.Tracing.Http
     {
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-        public Log4NetDelegatingHandler(bool enabled)
+        public Log4NetDelegatingHandler(bool logAll)
         {
-            LogRequest = LogRequestContent = LogResponse = LogResponseContent = enabled;
+            LogRequest = LogRequestContent = LogResponse = LogResponseContent = logAll;
+            AllowedHeaders = new List<string> { "Accept", "Content-Type", "X-CorrelationId", "X-RequestId" };
         }
 
+        public List<string> AllowedHeaders { get; private set; }
         public bool LogRequest { get; set; }
         public bool LogRequestContent { get; set; }
         public bool LogResponse { get; set; }
@@ -25,24 +31,56 @@ namespace Albumprinter.CorrelationTracking.Tracing.Http
         {
             if (LogRequest)
             {
-                var output = @"BeforeSendRequest: " + request;
-                if (LogRequestContent && request.Content != null)
+                var output = new StringBuilder();
+                output.Append(@"BeforeSendRequest: ");
+                output.Append("Method: ").Append(request.Method);
+                output.Append(", RequestUri: '").Append(request.RequestUri?.ToString() ?? "<null>");
+                output.Append("', Version: ").Append(request.Version);
+                output.AppendLine(", Headers: {");
+                output.AppendLine(GetHeaders(AllowedHeaders, request.Headers, request.Content?.Headers));
+                output.Append("}");
+                if (LogRequestContent)
                 {
-                    output = string.Concat(output, Environment.NewLine, await request.Content.ReadAsStringAsync().ConfigureAwait(false));
+                    output.Append(", Content: ");
+                    output.Append(request.Content == null ? "<null>" : await request.Content.ReadAsStringAsync().ConfigureAwait(false));
                 }
-                Log.Info(output);
+                Log.Info(output.ToString());
             }
             var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
             if (LogResponse)
             {
-                var output = @"AfterReceiveResponse: " + response;
-                if (LogResponseContent && response.Content != null)
+                var output = new StringBuilder();
+                output.Append("AfterReceiveResponse: ");
+                output.Append("StatusCode: ").Append((int) response.StatusCode);
+                output.Append(", ReasonPhrase: '").Append(response.ReasonPhrase ?? "<null>");
+                output.Append("', Version: ").Append(response.Version);
+                output.AppendLine(", Headers: {");
+                output.AppendLine(GetHeaders(AllowedHeaders, response.Headers, response.Content?.Headers));
+                output.Append("}");
+                if (LogResponseContent)
                 {
-                    output = string.Concat(output, Environment.NewLine, await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+                    output.Append(", Content: ");
+                    output.Append(response.Content == null ? "<null>" : await response.Content.ReadAsStringAsync().ConfigureAwait(false));
                 }
-                Log.Info(output);
+                Log.Info(output.ToString());
             }
             return response;
+        }
+
+        private static string GetHeaders(ICollection<string> allowedHeaders, HttpHeaders httpHeaders, HttpHeaders contentHeaders = null)
+        {
+            if (allowedHeaders == null)
+            {
+                throw new ArgumentNullException(nameof(allowedHeaders));
+            }
+            if (httpHeaders == null)
+            {
+                throw new ArgumentNullException(nameof(httpHeaders));
+            }
+            var headers = httpHeaders.Concat(contentHeaders ?? Enumerable.Empty<KeyValuePair<string, IEnumerable<string>>>());
+            return string.Join(
+                Environment.NewLine,
+                headers.Where(h => allowedHeaders.Contains(h.Key)).Select(h => $"{h.Key}: {string.Join(" ", h.Value)}"));
         }
     }
 }
--- src/Tracing.IIS/TrackingHttpModuleConfiguration.cs
+++ src/Tracing.IIS/TrackingHttpModuleConfiguration.cs
@@ -14,12 +14,8 @@ namespace Albumprinter.CorrelationTracking.Tracing.IIS
         public TrackingHttpModuleConfiguration(string allowedUrls, IEnumerable<string> allowedHeaders)
         {
             AllowedUrls = new Regex(allowedUrls ?? @"/api/|(\.asmx|\.svc)(\?|$)", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
-            AllowedHeaders = new List<string>(allowedHeaders ?? Enumerable.Empty<string>())
-            {
-                "Accept",
-                "Content-Type",
-                "X-CorrelationId"
-            };
+            AllowedHeaders = new List<string> { "Accept", "Content-Type", "X-CorrelationId", "X-RequestId" };
+            AllowedHeaders.AddRange(allowedHeaders ?? Enumerable.Empty<string>());
         }
 
         public static TrackingHttpModuleConfiguration FromConfig(string moduleName)
