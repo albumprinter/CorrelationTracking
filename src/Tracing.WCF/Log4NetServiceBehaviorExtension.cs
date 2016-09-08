c683b1bbf18553766291992f6721bfa050f83010
ivanakcheurov <ivan.akcheurov@albumprinter.com>
2016-09-08 18:33:31 +0200

    PLANTNPI-1688 PlantAPI should support CorrelationId Adding Log4NetClientBehaviorExtension

---
+++ src/Tracing.WCF/Log4NetServiceBehaviorExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ServiceModel.Configuration;
+
+namespace Albumprinter.CorrelationTracking.Tracing.WCF
+{
+    public sealed class Log4NetServiceBehaviorExtension : BehaviorExtensionElement
+    {
+        public override Type BehaviorType => typeof(Log4NetServiceBehavior);
+
+        protected override object CreateBehavior() => new Log4NetServiceBehavior();
+    }
+}
