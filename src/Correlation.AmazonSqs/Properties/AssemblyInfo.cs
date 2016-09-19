bf099682b87f675908e6cf453e994cb1ade47c73
ivanakcheurov <ivan.akcheurov@albumprinter.com>
2016-09-19 20:03:30 +0200

    PLANTNPI-1726 Enable CorrelationTracking to support Amazon SQS Adding nuget packages for SQS and fixing the AssemblyTitle

--- src/Correlation.AmazonSqs/Properties/AssemblyInfo.cs
+++ src/Correlation.AmazonSqs/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Albumprinter.CorrelationTracking.AmazonSqs")]
+[assembly: AssemblyTitle("Albumprinter.CorrelationTracking.Correlation.AmazonSqs")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("095ea02d-2da9-4dec-be88-ef8bb0f74dc5")]
