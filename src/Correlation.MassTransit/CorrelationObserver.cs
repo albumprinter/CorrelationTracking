a1ea11ead13e47cd0523bd3e4f87eb4bb14532f6
Unknown <sergii.kolbasin@albumprinter.com>
2016-11-23 17:59:31 +0100

    Attempt to restore CorrelationId during PreConsume

--- src/Correlation.MassTransit/CorrelationObserver.cs
+++ src/Correlation.MassTransit/CorrelationObserver.cs
@@ -3,17 +3,41 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Albumprinter.CorrelationTracking.Correlation.Core;
 using MassTransit;
+using MassTransit.Pipeline;
 
 namespace Albumprinter.CorrelationTracking.Correlation.MassTransit
 {
-    public sealed class CorrelationObserver : IPublishObserver, ISendObserver, IReceiveObserver
+    public sealed class CorrelationObserver : IPublishObserver, ISendObserver, IReceiveObserver, IConsumeObserver
     {
         private static readonly Task Done = Task.FromResult(true);
         public static readonly CorrelationObserver Instance = new CorrelationObserver();
 
+        public CorrelationObserver()
+        {
+            ReceiveContextManager = new ReceiveContextManager();
+            InjectOnPrePublish = true;
+            InjectOnPreSend = true;
+            InjectOnPreSendToRabbitMqProperties = true;
+            RestoreOnPreReceive = true;
+            RestoreOnPreConsume = true;
+        }
+
+        private ReceiveContextManager ReceiveContextManager { get; }
+        public bool InjectOnPrePublish { get; set; }
+        public bool InjectOnPreSend { get; set; }
+        /// <summary>
+        /// BUG: Masstransit.Rabbitmq v3.3.5 copies context.headers to transport.headers before calling PreSend method :(
+        /// </summary>
+        public bool InjectOnPreSendToRabbitMqProperties { get; set; }
+        public bool RestoreOnPreReceive { get; set; }
+        public bool RestoreOnPreConsume { get; set; }
+
         public Task PrePublish<T>(PublishContext<T> context) where T : class
         {
-            context.Headers.Set(CorrelationKeys.CorrelationId, CorrelationScope.Current.CorrelationId.ToString());
+            if (InjectOnPrePublish)
+            {
+                context.Headers.Set(CorrelationKeys.CorrelationId, CorrelationScope.Current.CorrelationId.ToString());
+            }
             return Done;
         }
 
@@ -29,15 +53,23 @@ namespace Albumprinter.CorrelationTracking.Correlation.MassTransit
 
         public Task PreSend<T>(SendContext<T> context) where T : class
         {
-            context.Headers.Set(CorrelationKeys.CorrelationId, CorrelationScope.Current.CorrelationId.ToString());
-
-            // BUG: Masstransit.Rabbitmq v3.3.5 copies context.headers to transport.headers before calling PreSend method :(
-            if (context.GetType().Name.StartsWith("RabbitMq", StringComparison.OrdinalIgnoreCase))
+            if (InjectOnPreSend)
             {
-                var headers = ((dynamic) context).BasicProperties?.Headers as IDictionary<string, object>;
-                headers?.Add(CorrelationKeys.CorrelationId, CorrelationScope.Current.CorrelationId.ToString());
-            }
+                context.Headers.Set(CorrelationKeys.CorrelationId, CorrelationScope.Current.CorrelationId.ToString());
 
+                if (InjectOnPreSendToRabbitMqProperties)
+                {
+                    // BUG: Masstransit.Rabbitmq v3.3.5 copies context.headers to transport.headers before calling PreSend method :(
+                    if (context.GetType().Name.StartsWith("RabbitMq", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var headers = ((dynamic) context).BasicProperties?.Headers as IDictionary<string, object>;
+                        if (headers != null)
+                        {
+                            headers[CorrelationKeys.CorrelationId] = CorrelationScope.Current.CorrelationId.ToString();
+                        }
+                    }
+                }
+            }
             return Done;
         }
 
@@ -53,14 +85,12 @@ namespace Albumprinter.CorrelationTracking.Correlation.MassTransit
 
         Task IReceiveObserver.PreReceive(ReceiveContext context)
         {
-            object value;
-            var correlationId = context.TransportHeaders.TryGetHeader(CorrelationKeys.CorrelationId, out value) ? Guid.Parse((string)value) : Guid.NewGuid();
-            CorrelationManager.Instance.UseScope(correlationId);
-            return Done;
-        }
-
-        Task IReceiveObserver.PostReceive(ReceiveContext context)
-        {
+            if (RestoreOnPreReceive)
+            {
+                object value;
+                var correlationId = context.TransportHeaders.TryGetHeader(CorrelationKeys.CorrelationId, out value) ? Guid.Parse((string)value) : Guid.NewGuid();
+                ReceiveContextManager.Capture(context, CorrelationManager.Instance.UseScope(correlationId));
+            }
             return Done;
         }
 
@@ -74,9 +104,49 @@ namespace Albumprinter.CorrelationTracking.Correlation.MassTransit
             return Done;
         }
 
+        Task IReceiveObserver.PostReceive(ReceiveContext context)
+        {
+            ReceiveContextManager.Release(context);
+            return Done;
+        }
+
         Task IReceiveObserver.ReceiveFault(ReceiveContext context, Exception exception)
         {
+            ReceiveContextManager.Release(context);
             return Done;
+        }
+
+        Task IConsumeObserver.PreConsume<T>(ConsumeContext<T> context)
+        {
+            if (RestoreOnPreConsume)
+            {
+                // NOTE: The last chance to restore CorrelationId from the headers of message.
+                object value;
+                var correlationScope = CorrelationScope.Current;
+                var correlationId =
+                        context.Headers.TryGetHeader(CorrelationKeys.CorrelationId, out value) ||
+                        context.ReceiveContext.TransportHeaders.TryGetHeader(CorrelationKeys.CorrelationId, out value)
+                            ? Guid.Parse((string) value)
+                            : Guid.NewGuid();
+                if (correlationId != correlationScope.CorrelationId)
+                {
+                    ReceiveContextManager.Release(context.ReceiveContext);
+                    // NOTE: RequestId to link all related logs before reassigning the scope.
+                    var requestId = correlationScope.RequestId != Guid.Empty ? correlationScope.RequestId : Guid.NewGuid();
+                    ReceiveContextManager.Capture(context.ReceiveContext, CorrelationManager.Instance.UseScope(correlationId, requestId));
+                }
+            }
+            return context.CompleteTask;
+        }
+
+        Task IConsumeObserver.PostConsume<T>(ConsumeContext<T> context)
+        {
+            return context.CompleteTask;
+        }
+
+        Task IConsumeObserver.ConsumeFault<T>(ConsumeContext<T> context, Exception exception)
+        {
+            return context.CompleteTask;
         }
     }
 }
---
+++ src/Correlation.MassTransit/ReceiveContextManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using MassTransit;
+
+namespace Albumprinter.CorrelationTracking.Correlation.MassTransit
+{
+    internal sealed class ReceiveContextManager
+    {
+        private readonly ConditionalWeakTable<ReceiveContext, IDisposable> weakTable;
+
+        public ReceiveContextManager()
+        {
+            weakTable = new ConditionalWeakTable<ReceiveContext, IDisposable>();
+        }
+
+        public void Capture(ReceiveContext context, IDisposable disposable)
+        {
+            weakTable.Add(context, disposable);
+        }
+
+        public void Release(ReceiveContext context)
+        {
+            IDisposable disposable;
+            if (weakTable.TryGetValue(context, out disposable))
+            {
+                weakTable.Remove(context);
+                disposable?.Dispose();
+            }
+        }
+    }
+}
