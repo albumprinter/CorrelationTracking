2f2b956adfcf948586389c275de8e7cb93d674d3
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2017-11-21 17:20:00 +0100

    WATSON-904 Add sns client with correlation id

---
+++ src/Correlation.AmazonSns/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Albumprinter.CorrelationTracking.Correlation.AmazonSns")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("64d85298-47c4-4b98-9220-7974cfa0ab16")]
