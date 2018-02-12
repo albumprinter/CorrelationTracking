28c0fadc490bfe33033494f9f677a3af345bed89
kvasnyj <kvasnyj@gmail.com>
2018-02-12 11:34:21 +0100

    Added Correlation.Core.Standard project and move classes from Correlation.Core

---
+++ src/Correlation.Core.Standard/CorrelationKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace Albumprinter.CorrelationTracking.Correlation.Core
+{
+    public static class CorrelationKeys
+    {
+        public static readonly string Namespace = @"http://schemas.microsoft.com/2004/09/ServiceModel/Diagnostics";
+        public static readonly string ProcessId = @"X-ProcessId";
+        public static readonly string CorrelationId = @"X-CorrelationId";
+        public static readonly string RequestId = @"X-RequestId";
+        public static readonly string ActivityId = @"X-ActivityId";
+    }
+}
---
+++ src/Correlation.Core.Standard/ICorrelationScopeInterceptor.cs
@@ -0,0 +1,8 @@
+﻿namespace Albumprinter.CorrelationTracking.Correlation.Core
+{
+    public interface ICorrelationScopeInterceptor
+    {
+        void Enter(CorrelationManager manager, CorrelationScope scope);
+        void Exit(CorrelationManager manager, CorrelationScope scope);
+    }
+}
