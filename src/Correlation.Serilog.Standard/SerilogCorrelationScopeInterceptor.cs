06098f42d137d1bd0b31fe3b52bae10abdb9e1cc
Martijn Mol <martijn.mol@albumprinter.com>
2018-03-12 14:50:09 +0100

    WATSON-1161 add support for Serilog

---
+++ src/Correlation.Serilog.Standard/SerilogCorrelationScopeInterceptor.cs
@@ -0,0 +1,26 @@
+using Albumprinter.CorrelationTracking.Correlation.Core;
+using Serilog.Context;
+
+namespace Correlation.Serilog.Standard
+{
+    public sealed class SerilogCorrelationScopeInterceptor : ICorrelationScopeInterceptor
+    {
+        public void Enter(CorrelationManager manager, CorrelationScope scope)
+        {
+            SetLogicalProperties(scope);
+        }
+
+        public void Exit(CorrelationManager manager, CorrelationScope scope)
+        {
+            SetLogicalProperties(scope);
+        }
+
+        private static void SetLogicalProperties(CorrelationScope scope)
+        {
+            LogContext.PushProperty(CorrelationKeys.ProcessId, scope.ProcessId);
+            LogContext.PushProperty(CorrelationKeys.CorrelationId, scope.CorrelationId);
+            LogContext.PushProperty(CorrelationKeys.RequestId, scope.RequestId);
+            LogContext.PushProperty(CorrelationKeys.ActivityId, System.Diagnostics.Trace.CorrelationManager.ActivityId);
+        }
+    }
+}
