cfe75c8abdf627458d642f78d9ef24ce9da3b3d5
gmariano <giuseppe.mariano@albelli.com>
2018-03-13 12:00:57 +0100

    WATSON-1161 Add CorrelationDecorator constructors

--- src/Correlation.AmazonSns.Standard/CorrelationDecorator.cs
+++ src/Correlation.AmazonSns.Standard/CorrelationDecorator.cs
@@ -2,6 +2,8 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Albumprinter.CorrelationTracking.Correlation.Core;
+using Amazon;
+using Amazon.Runtime;
 using Amazon.SimpleNotificationService;
 using Amazon.SimpleNotificationService.Model;
 
@@ -9,6 +11,54 @@ namespace Albumprinter.CorrelationTracking.Correlation.AmazonSns
 {
     public class CorrelationDecorator : AmazonSimpleNotificationServiceClient
     {
+        public CorrelationDecorator()
+        {
+        }
+
+        public CorrelationDecorator(RegionEndpoint region) : base(region)
+        {
+        }
+
+        public CorrelationDecorator(AmazonSimpleNotificationServiceConfig config) : base(config)
+        {
+        }
+
+        public CorrelationDecorator(AWSCredentials credentials) : base(credentials)
+        {
+        }
+
+        public CorrelationDecorator(AWSCredentials credentials, RegionEndpoint region) : base(credentials, region)
+        {
+        }
+
+        public CorrelationDecorator(AWSCredentials credentials, AmazonSimpleNotificationServiceConfig clientConfig) : base(credentials, clientConfig)
+        {
+        }
+
+        public CorrelationDecorator(string awsAccessKeyId, string awsSecretAccessKey) : base(awsAccessKeyId, awsSecretAccessKey)
+        {
+        }
+
+        public CorrelationDecorator(string awsAccessKeyId, string awsSecretAccessKey, RegionEndpoint region) : base(awsAccessKeyId, awsSecretAccessKey, region)
+        {
+        }
+
+        public CorrelationDecorator(string awsAccessKeyId, string awsSecretAccessKey, AmazonSimpleNotificationServiceConfig clientConfig) : base(awsAccessKeyId, awsSecretAccessKey, clientConfig)
+        {
+        }
+
+        public CorrelationDecorator(string awsAccessKeyId, string awsSecretAccessKey, string awsSessionToken) : base(awsAccessKeyId, awsSecretAccessKey, awsSessionToken)
+        {
+        }
+
+        public CorrelationDecorator(string awsAccessKeyId, string awsSecretAccessKey, string awsSessionToken, RegionEndpoint region) : base(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, region)
+        {
+        }
+
+        public CorrelationDecorator(string awsAccessKeyId, string awsSecretAccessKey, string awsSessionToken, AmazonSimpleNotificationServiceConfig clientConfig) : base(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, clientConfig)
+        {
+        }
+
         public override Task<PublishResponse> PublishAsync(PublishRequest request, CancellationToken cancellationToken = new CancellationToken())
         {
             AddCorrelationAttributeIfAbsent(request);
