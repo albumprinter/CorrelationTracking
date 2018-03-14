ab622e714144ec28ef896a50f79a9191514d3c84
gmariano <giuseppe.mariano@albelli.com>
2018-03-14 13:14:28 +0100

    WATSON-1161 Clean code

--- src/Correlation.AmazonSns. to be deleted/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Albumprinter.CorrelationTracking.Correlation.AmazonSns")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("64d85298-47c4-4b98-9220-7974cfa0ab16")]
--- src/Correlation.Core.to be deleted/ICorrelationScopeInterceptor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Albumprinter.CorrelationTracking.Correlation.Core
-{
-    public interface ICorrelationScopeInterceptor
-    {
-        void Enter(CorrelationManager manager, CorrelationScope scope);
-        void Exit(CorrelationManager manager, CorrelationScope scope);
-    }
-}
