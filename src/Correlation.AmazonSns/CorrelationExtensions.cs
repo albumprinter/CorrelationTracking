76715cf3f1a911bd0b1d6cb82fe6270431d9f151
gmariano <giuseppe.mariano@albelli.com>
2018-03-21 12:56:48 +0100

    WATSON-1197 Add method to extract correlatioId from SNSEvent

---
+++ src/Correlation.AmazonSns/CorrelationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Albumprinter.CorrelationTracking.Correlation.Core;
+using Amazon.Lambda.SNSEvents;
+
+namespace Albumprinter.CorrelationTracking.Correlation.AmazonSns
+{
+    public static class CorrelationExtensions
+    {
+        public static Guid? ExtractCorrelationId(this SNSEvent.SNSMessage snsMessage)
+        {
+            SNSEvent.MessageAttribute attribute;
+            var correlationId =
+                snsMessage.MessageAttributes.TryGetValue(CorrelationKeys.CorrelationId, out attribute)
+                    ? Guid.Parse(attribute.Value)
+                    : (Guid?) null;
+            return correlationId;
+        }
+    }
+}
