3536e45a04afafbe637141d1f9f2ae2c362b001b
gmariano <giuseppe.mariano@albelli.com>
2018-03-23 11:27:52 +0100

    WATSON-1197 Move CorrelationExtensions for SNSMessage on a separate project

--- src/Correlation.AmazonSns.Lambda/CorrelationExtensions.cs
+++ src/Correlation.AmazonSns.Lambda/CorrelationExtensions.cs
@@ -2,7 +2,7 @@
 using Albumprinter.CorrelationTracking.Correlation.Core;
 using Amazon.Lambda.SNSEvents;
 
-namespace Albumprinter.CorrelationTracking.Correlation.AmazonSns
+namespace Albumprinter.CorrelationTracking.Correlation.AmazonSns.Lambda
 {
     public static class CorrelationExtensions
     {
