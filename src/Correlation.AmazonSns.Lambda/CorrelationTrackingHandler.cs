e45380d3127095bf92d7659362a14eb8a1f94e3f
gmariano <giuseppe.mariano@albelli.com>
2018-03-26 14:37:59 +0200

    Added Correlation Tracking for SNSRecord handling

---
+++ src/Correlation.AmazonSns.Lambda/CorrelationTrackingHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Albumprinter.CorrelationTracking.Correlation.AmazonSns.Lambda.Logging;
+using Albumprinter.CorrelationTracking.Correlation.Core;
+using Amazon.Lambda.SNSEvents;
+
+namespace Albumprinter.CorrelationTracking.Correlation.AmazonSns.Lambda
+{
+    public class CorrelationTrackingHandler : ICorrelationTrackingHandler
+    {
+        private static readonly ILog _logger = LogProvider.GetLogger(typeof(CorrelationTrackingHandler));
+        private readonly IServiceProvider _serviceProvider;
+
+        public CorrelationTrackingHandler(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        }
+
+        public async Task Handle(SNSEvent snsEvent)
+        {
+            var requestId = Guid.NewGuid();
+
+            foreach (var snsRecord in snsEvent.Records)
+            {
+                using (CorrelationManager.Instance.UseScope(snsRecord.Sns.ExtractCorrelationId() ?? Guid.NewGuid(), requestId))
+                {
+                    _logger.Info("Getting message handler");
+                    var handler = (ISnsRecordHandler)_serviceProvider.GetService(typeof(ISnsRecordHandler));
+                    if (handler == null)
+                    {
+                        _logger.Fatal($"No ISnsRecordHandler<{typeof(ISnsRecordHandler).Name}> could be found.");
+                        throw new InvalidOperationException($"No INotificationHandler<{typeof(ISnsRecordHandler).Name}> could be found.");
+                    }
+                    _logger.Info("Invoking message handler");
+                    await handler.Handle(snsRecord).ConfigureAwait(false);
+                }
+            }
+        }
+    }
+}
