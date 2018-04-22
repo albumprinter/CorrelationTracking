dd4e50507585b3ed59fc8557978dcaadb3c95d70
Antonio Neto <antonio.bengelsdorff@gmail.com>
2018-04-22 20:13:57 +0200

    Add extension loggerConfiguration.AddCorrelationProperties()

---
+++ src/Correlation.Serilog/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using Albumprinter.CorrelationTracking.Correlation.Core;
+using Serilog;
+
+namespace Albumprinter.CorrelationTracking.Correlation.Serilog
+{
+    public static class Extensions
+    {
+        public static LoggerConfiguration AddCorrelationProperties(this LoggerConfiguration loggeerConfiguration)
+        {
+            CorrelationManager.Instance.ScopeInterceptors.Add(new SerilogCorrelationScopeInterceptor());
+            return loggeerConfiguration;
+        }
+    }
+}
