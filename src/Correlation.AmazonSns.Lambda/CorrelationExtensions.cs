8e3efec22bad7bf9047decda448228a22090dd8c
Antonio Bengelsdorff <antonio.bengelsdorff@gmail.com>
2018-04-23 10:12:19 +0200

    Refactor sns lambda (#36)
    * Cleanup References
    
    * Refactor ExtractCorrelationId to avoid NRE
    
    * Add extension snsRecord.TrackCorrelationId()
    
    * Add extension loggerConfiguration.AddCorrelationProperties()
    
    * Refactor ExtractCorrelationId to avoid NRE

--- src/Correlation.AmazonSns.Lambda/CorrelationExtensions.cs
+++ src/Correlation.AmazonSns.Lambda/CorrelationExtensions.cs
@@ -8,12 +8,19 @@ namespace Albumprinter.CorrelationTracking.Correlation.AmazonSns.Lambda
     {
         public static Guid? ExtractCorrelationId(this SNSEvent.SNSMessage snsMessage)
         {
-            SNSEvent.MessageAttribute attribute;
-            var correlationId =
-                snsMessage.MessageAttributes.TryGetValue(CorrelationKeys.CorrelationId, out attribute)
-                    ? Guid.Parse(attribute.Value)
-                    : (Guid?) null;
-            return correlationId;
+            if (snsMessage?.MessageAttributes != null &&
+                snsMessage.MessageAttributes.TryGetValue(CorrelationKeys.CorrelationId, out var attribute) &&
+                attribute?.Value != null &&
+                Guid.TryParse(attribute.Value, out var correlationId))
+            {
+                return correlationId;
+            }
+            return null;
+        }
+
+        public static IDisposable TrackCorrelationId(this SNSEvent.SNSRecord snsRecord)
+        {
+            return CorrelationManager.Instance.UseScope(snsRecord.Sns.ExtractCorrelationId() ?? Guid.NewGuid(), Guid.NewGuid());
         }
     }
 }
