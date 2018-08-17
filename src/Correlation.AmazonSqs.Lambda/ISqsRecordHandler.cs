02b2564388128063ea9dc0231ef089b05a33dcf7
gmariano <giuseppe.mariano@albelli.com>
2018-08-17 16:18:33 +0200

    LUIGI-111 Add support for Amazon.Lambda.SQSEvents

---
+++ src/Correlation.AmazonSqs.Lambda/ISqsRecordHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Amazon.Lambda.SQSEvents;
+
+namespace Albumprinter.CorrelationTracking.Correlation.AmazonSqs.Lambda
+{
+    public interface ISqsRecordHandler
+    {
+        Task Handle(SQSEvent.SQSMessage sqsMessage);
+    }
+}
