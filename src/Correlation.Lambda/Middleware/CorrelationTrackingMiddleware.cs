6204a2cd0df37b72224b1593cc7bd00f42ed7004
Ruslan Hamzatov <ruslan.hamzatov@albelli.com>
2019-04-05 14:21:42 +0200

    added new project for .net core support

---
+++ src/Correlation.Lambda/Middleware/CorrelationTrackingMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Albumprinter.CorrelationTracking.Correlation.Core;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Albumprinter.CorrelationTracking.Correlation.Middleware
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class CorrelationTrackingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CorrelationTrackingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Headers.TryGetValue(CorrelationKeys.CorrelationId, out var headers)
+                && headers.Count >= 1
+                && Guid.TryParse(headers[0], out var correlationId))
+            {
+                CorrelationManager.Instance.UseScope(correlationId, Guid.NewGuid());
+            }
+            else
+            {
+                CorrelationManager.Instance.UseScope(Guid.NewGuid(), Guid.NewGuid());
+            }
+
+            await _next(context);
+        }
+    }
+}
