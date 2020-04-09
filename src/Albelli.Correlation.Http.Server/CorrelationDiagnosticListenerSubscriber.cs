87c6fef3c475a06b77c85393af286e190b19e397
Denys Zhuravel <denys.zhuravel@albelli.com>
2020-04-09 09:56:52 +0200

    Force W3C correlation format and try to use the W3C values for backwards compatibility with our system

--- src/Albelli.Correlation.Http.Server/CorrelationDiagnosticListenerSubscriber.cs
+++ src/Albelli.Correlation.Http.Server/CorrelationDiagnosticListenerSubscriber.cs
@@ -2,7 +2,6 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using Albumprinter.CorrelationTracking.Correlation.Core;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Http;
@@ -18,6 +17,12 @@ namespace Albelli.Correlation.Http.Server
         private readonly ActivitySpanId EMPTY_SPAN = ActivitySpanId.CreateFromString("ffffffffffffffff".AsSpan());
         private readonly ConcurrentDictionary<HttpContext, IDisposable> _ctxToDispose = new ConcurrentDictionary<HttpContext, IDisposable>();
 
+        public CorrelationDiagnosticListenerSubscriber()
+        {
+            // We want to use the W3C format so we can be compatible with the standard as much as possible.
+            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+        }
+
         public void OnCompleted() { }
 
         public void OnError(Exception error) { }
@@ -26,19 +31,42 @@ namespace Albelli.Correlation.Http.Server
         {
             if (ctx == null) return;
 
-            var id = ResolveCorrelationId(ctx);
+            var resolvedBackwardsCompatibleId = TryResolveCorrelationId(ctx, out var correlationId);
 
             // We are only going to set the Activity's correlation parent
             // if we don't have an existing parent already
             // and we have one that came from albelli's correlation.
             if (Activity.Current != null
                 && Activity.Current.Parent == null
-                && id != null)
+                && resolvedBackwardsCompatibleId)
             {
-                Activity.Current.SetParentId(ActivityTraceId.CreateFromString(id.Value.ToString("N").AsSpan()), EMPTY_SPAN);
+                Activity.Current.SetParentId(ActivityTraceId.CreateFromString(correlationId.ToString("N").AsSpan()), EMPTY_SPAN);
             }
-            _ctxToDispose[ctx] = CorrelationManager.Instance.UseScope(id ?? Guid.NewGuid());
 
+            // We need to set some kind of id for the old system.
+            // So, we will try to use the trace-id from the W3C standard if we can use it
+            // Otherwise we will just generate a new guid instead
+            if (!resolvedBackwardsCompatibleId)
+            {
+                if (Activity.Current != null && Activity.DefaultIdFormat == ActivityIdFormat.W3C)
+                {
+                    var currentTraceId = Activity.Current.TraceId.ToHexString();
+                    if (Guid.TryParse(currentTraceId, out var w3CTraceIdAsGuid) && w3CTraceIdAsGuid != Guid.Empty)
+                    {
+                        correlationId = w3CTraceIdAsGuid;
+                    }
+                    else
+                    {
+                        correlationId = Guid.NewGuid();
+                    }
+                }
+                else
+                {
+                    correlationId = Guid.NewGuid();
+                }
+            }
+
+            _ctxToDispose[ctx] = CorrelationManager.Instance.UseScope(correlationId);
         }
 
         private void Stop(HttpContext ctx)
@@ -62,16 +90,18 @@ namespace Albelli.Correlation.Http.Server
             }
         }
 
-        private Guid? ResolveCorrelationId(HttpContext context)
+        private bool TryResolveCorrelationId(HttpContext context, out Guid id)
         {
             if (context.Request.Headers.TryGetValue(CorrelationKeys.CorrelationId, out var headers)
                 && headers.Count >= 1
                 && Guid.TryParse(headers[0], out var correlationId))
             {
-                return correlationId;
+                id = correlationId;
+                return true;
             }
 
-            return null;
+            id = Guid.Empty;
+            return false;
         }
     }
 }
