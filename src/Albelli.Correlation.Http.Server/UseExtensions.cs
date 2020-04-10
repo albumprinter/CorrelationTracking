7bc4887af2101d09e8ac920f787f4134bac30171
Denys Zhuravel <denys.zhuravel@albelli.com>
2020-04-10 17:27:23 +0200

    Use the new decorating logger instead of the whole factory

--- src/Albelli.Correlation.Http.Server/UseExtensions.cs
+++ src/Albelli.Correlation.Http.Server/UseExtensions.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using Albelli.Correlation.Http.Server.Middleware;
 using Albelli.CorrelationTracking.Correlation.Logging;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.ObjectPool;
 
 namespace Albelli.Correlation.Http.Server
 {
@@ -16,7 +15,7 @@ namespace Albelli.Correlation.Http.Server
     public static class UseExtensions
     {
         /// <summary>
-        /// Adds a decorator for ILoggerFactory that can decorate log messages
+        /// Adds a replacement for ILoggerT that can decorate log messages
         /// with values from Activity.Current.Baggage and Activity.Current.Tags.
         ///
         /// It will log:
@@ -26,11 +25,11 @@ namespace Albelli.Correlation.Http.Server
         /// <param name="services">The service collection from the IoC container</param>
         public static void AddDefaultActivityBagTagLoggerFactory([NotNull] this IServiceCollection services)
         {
-            services.AddActivityBagTagLoggerFactory(s => s.StartsWith("X-", StringComparison.OrdinalIgnoreCase), _ => true);
+            services.AddActivityBagTagLogger(s => s.StartsWith("X-", StringComparison.OrdinalIgnoreCase), _ => true);
         }
 
         /// <summary>
-        /// Adds a decorator for ILoggerFactory that can decorate log messages
+        /// Adds a replacement for ILoggerT that can decorate log messages
         /// with values from Activity.Current.Baggage and Activity.Current.Tags.
         ///
         /// At least one predicate must be specified.
@@ -44,13 +43,15 @@ namespace Albelli.Correlation.Http.Server
         /// A predicate which checks whether this tag should be logged or not.
         /// `null` means it's not going to be logged.
         /// </param>
-        public static void AddActivityBagTagLoggerFactory([NotNull] this IServiceCollection services, Predicate<string> shouldLogBag = null, Predicate<string> shouldLogTag = null)
+        public static void AddActivityBagTagLogger([NotNull] this IServiceCollection services, Predicate<string> shouldLogBag = null, Predicate<string> shouldLogTag = null)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
-            if (shouldLogBag == null && shouldLogTag == null)
-                throw new ArgumentException("At least one predicate should be specified");
-
-            services.Decorate<ILoggerFactory, ActivityBagTagLoggerFactory>(shouldLogBag, shouldLogTag);
+            services.AddSingleton(new ActivityBagTagDecoratingLoggerSettings(shouldLogBag, shouldLogTag));
+            var existingLifetime = services
+                .Where(x => x.ServiceType == typeof(ILogger<>))
+                .Select(x => x.Lifetime)
+                .FirstOrDefault(); // If the existing lifetime was not found, it defaults to Singleton
+            services.Replace(new ServiceDescriptor(typeof(ILogger<>), typeof(ActivityBagTagDecoratingLogger<>), existingLifetime));
         }
 
         /// <summary>
@@ -73,85 +74,6 @@ namespace Albelli.Correlation.Http.Server
             app.UseMiddleware<CorrelationTrackingMiddleware>();
             app.UseMiddleware<LogRequestMiddleware>();
             app.UseMiddleware<LogResponseMiddleware>();
-        }
-    }
-
-    internal static class ServiceCollectionExtensions
-    {
-        public static IServiceCollection Decorate<TService, TDecorator>(this IServiceCollection services, params object[] additionalParameters)
-            where TDecorator : TService
-        {
-            return services.DecorateDescriptors(typeof(TService), x => x.Decorate(typeof(TDecorator), additionalParameters));
-        }
-
-        public static IServiceCollection Decorate<TService, TDecorator>(this IServiceCollection services)
-            where TDecorator : TService
-        {
-            return services.DecorateDescriptors(typeof(TService), x => x.Decorate(typeof(TDecorator)));
-        }
-
-        private static IServiceCollection DecorateDescriptors(this IServiceCollection services, Type serviceType, Func<ServiceDescriptor, ServiceDescriptor> decorator)
-        {
-            var descriptors = services.Where(service => service.ServiceType == serviceType).ToArray();
-            if (descriptors.Length == 0)
-            {
-                throw new InvalidOperationException("Can't find the type to decorate");
-            }
-
-            foreach (var descriptor in descriptors)
-            {
-                var index = services.IndexOf(descriptor);
-                services[index] = decorator(descriptor);
-            }
-
-            return services;
-        }
-
-        private static ServiceDescriptor Decorate(this ServiceDescriptor descriptor, Type decoratorType, object[] additionalParameters)
-        {
-            return descriptor.WithFactory(provider =>
-            {
-                var parameters = new object[additionalParameters.Length + 1];
-                parameters[0] = provider.GetInstance(descriptor);
-                for (int i = 0; i < additionalParameters.Length; i++)
-                    parameters[i + 1] = additionalParameters[i];
-                return provider.CreateInstance(decoratorType, parameters);
-            });
-        }
-
-        private static ServiceDescriptor Decorate(this ServiceDescriptor descriptor, Type decoratorType)
-        {
-            return descriptor.WithFactory(provider => provider.CreateInstance(decoratorType, provider.GetInstance(descriptor)));
-        }
-
-        private static ServiceDescriptor WithFactory(this ServiceDescriptor descriptor, Func<IServiceProvider, object> factory)
-        {
-            return ServiceDescriptor.Describe(descriptor.ServiceType, factory, descriptor.Lifetime);
-        }
-
-        private static object GetInstance(this IServiceProvider provider, ServiceDescriptor descriptor)
-        {
-            if (descriptor.ImplementationInstance != null)
-            {
-                return descriptor.ImplementationInstance;
-            }
-
-            if (descriptor.ImplementationType != null)
-            {
-                return provider.GetServiceOrCreateInstance(descriptor.ImplementationType);
-            }
-
-            return descriptor.ImplementationFactory(provider);
-        }
-
-        private static object GetServiceOrCreateInstance(this IServiceProvider provider, Type type)
-        {
-            return ActivatorUtilities.GetServiceOrCreateInstance(provider, type);
-        }
-
-        private static object CreateInstance(this IServiceProvider provider, Type type, params object[] arguments)
-        {
-            return ActivatorUtilities.CreateInstance(provider, type, arguments);
         }
     }
 }
---
+++ src/Correlation.Logging/ActivityBagTagDecoratingLogger.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
+
+namespace Albelli.CorrelationTracking.Correlation.Logging
+{
+    [PublicAPI]
+    public sealed class ActivityBagTagDecoratingLogger<T> : ILogger<T>
+    {
+        [NotNull] private readonly ActivityBagTagDecoratingLoggerSettings _settings;
+        [NotNull] private readonly ILogger _originalLogger;
+
+        public ActivityBagTagDecoratingLogger([NotNull] ILoggerFactory factory, [NotNull] ActivityBagTagDecoratingLoggerSettings activityBagTagDecoratingLoggerSettings)
+        {
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
+            _settings = activityBagTagDecoratingLoggerSettings ?? throw new ArgumentNullException(nameof(activityBagTagDecoratingLoggerSettings));
+            _originalLogger = factory.CreateLogger(nameof(T));
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var currentActivity = Activity.Current;
+            if (currentActivity == null)
+            {
+                _originalLogger.Log(logLevel, eventId, state, exception, formatter);
+                return;
+            }
+
+            Dictionary<string, object> correlationProperties = new Dictionary<string, object>();
+
+            if (_settings.ShouldLogBag != null)
+            {
+                var tagValues = currentActivity.Baggage
+                    .Where(x => _settings.ShouldLogBag(x.Key))
+                    .GroupBy(x => x.Key)
+                    .Select(group => group.First());
+                foreach (var keyValuePair in tagValues)
+                {
+                    correlationProperties[keyValuePair.Key] = keyValuePair.Value;
+                }
+            }
+
+            if (_settings.ShouldLogTag != null)
+            {
+                var tagValues = currentActivity.Tags
+                    .Where(x => _settings.ShouldLogTag(x.Key))
+                    .GroupBy(x => x.Key)
+                    .Select(group => group.First());
+                foreach (var keyValuePair in tagValues)
+                {
+                    correlationProperties[keyValuePair.Key] = keyValuePair.Value;
+                }
+            }
+
+            if (correlationProperties.Any())
+            {
+                using (_originalLogger.BeginScope(correlationProperties))
+                {
+                    _originalLogger.Log(logLevel, eventId, state, exception, formatter);
+                }
+            }
+            else
+            {
+                _originalLogger.Log(logLevel, eventId, state, exception, formatter);
+            }
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return _originalLogger.IsEnabled(logLevel);
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return _originalLogger.BeginScope(state);
+        }
+    }
+
+    public sealed class ActivityBagTagDecoratingLoggerSettings
+    {
+        public Predicate<string> ShouldLogBag { get; }
+        public Predicate<string> ShouldLogTag { get; }
+
+        public ActivityBagTagDecoratingLoggerSettings(Predicate<string> shouldLogBag, Predicate<string> shouldLogTag)
+        {
+            if (shouldLogBag == null && shouldLogTag == null)
+                throw new ArgumentException("At least one predicate should be specified");
+
+            ShouldLogBag = shouldLogBag;
+            ShouldLogTag = shouldLogTag;
+        }
+    }
+}
